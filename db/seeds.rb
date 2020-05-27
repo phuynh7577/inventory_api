c497732807d0f6156187470a6aeff34baabfa26b
Phi Huynh <phuynh7577@gmail.com>
2020-05-26 22:26:38 -0400

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-
-
-
-Inventory.create([
-    # {category: "beer", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 1},
-    {category: "wine", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1900.00, user_id: 2},
-    {category: "hard liquor", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 1},
-    {category: "wine", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1800.00, user_id: 3},
-    {category: "beer", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 2},
-    {category: "beer", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1400.00, user_id: 4},
-    {category: "hard liquor", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1200.00, user_id: 5},
-    {category: "wine", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1100.00, user_id: 2},
-    {category: "hard liquor", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 3},
+User.create([
+    {email: "vukidd@yahoo.com", password: "Benjamin1!", store_name: "Lets Rock and Roll"}
 ])
+
+
+# Inventory.create([
+#     # {category: "beer", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 1},
+#     {category: "wine", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1900.00, user_id: 2},
+#     {category: "hard liquor", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 1},
+#     {category: "wine", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1800.00, user_id: 3},
+#     {category: "beer", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 2},
+#     {category: "beer", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1400.00, user_id: 4},
+#     {category: "hard liquor", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1200.00, user_id: 5},
+#     {category: "wine", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1100.00, user_id: 2},
+#     {category: "hard liquor", qty: 100, name: "CORS Light 30rack", price_per_item: 26.99, total_cost: 1600.00, user_id: 3},
+# ])
